152cb2f84bf70c8b2ad0f2ce063fdc0fce79a7eb
Samson Elazakemhe <samson.elazakemhe@phed.com.ng>
2022-09-09 19:20:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const blogRoutes = require('./Routes/blogRoutes')
+
+
+const app = express();
+
+const dbURI = 'mongodb+srv://SammyBlog:Sylvasam18@cluster0.kotie.mongodb.net/Blog?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => app.listen(3000))
+.catch((err) => console.error(err));
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/add-blog', (req,res) => {
+ const blog = new Blog({
+     title: 'newest blog',
+     snippet: 'a new blog',
+     body: 'more about my new blog'
+ });
+ blog.save()
+ .then((result) => {
+     res.send(result)
+ })
+ .catch((err) => {
+     console.log(err)
+ })
+})
+
+
+app.get('/single-blog', (req,res) => {
+    Blog.findById('62025f4e82432c194bf44b7f')
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => console.log(err))
+})
+
+
+
+app.get('/', (req,res) => {
+    res.redirect('/blogs')
+});
+
+
+
+app.get('/about', (req,res) => {
+    res.render('about')
+});
+
+app.use('/blogs', blogRoutes)
+
+app.use((req,res) => {
+    res.status(404).render('404')
+});
